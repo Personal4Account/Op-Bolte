20c80dc6965e429e6e05a56f1139667ba3318720
Personal4Account <104253971+Personal4Account@users.noreply.github.com>
2022-04-27 14:40:22 +0530

    Update callback.py

--- handlers/callback.py
+++ handlers/callback.py
@@ -1,1 +1,7 @@
-
+from pyrogram import Client, filters
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+from config import (
+    BOT_USERNAME,
+    GROUP_SUPPORT,
+    UPDATES_CHANNEL,
+)
