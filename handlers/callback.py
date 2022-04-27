de74dc90a5215e94ed170b016622bb12f36735f5
Personal4Account <104253971+Personal4Account@users.noreply.github.com>
2022-04-27 14:43:57 +0530

    Update callback.py

--- handlers/callback.py
+++ handlers/callback.py
@@ -5,3 +5,29 @@ from config import (
     GROUP_SUPPORT,
     UPDATES_CHANNEL,
 )
+
+
+@Client.on_callback_query(filters.regex("command_list"))
+@check_blacklist()
+async def commands_set(_, query: CallbackQuery):
+    user_id = query.from_user.id
+    await query.answer("commands menu")
+    await query.edit_message_text(
+        f"""✨ **Hello [{query.message.chat.first_name}](tg://user?id={query.message.chat.id}) !**
+» Check out the menu below to read the module information & see the list of available Commands !
+All commands can be used with (`! / .`) handler""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("👮🏻‍♀️ Admins Commands", callback_data="admin_command"),
+                ],[
+                    InlineKeyboardButton("👩🏻‍💼 Users Commands", callback_data="user_command"),
+                ],[
+                    InlineKeyboardButton("Sudo Commands", callback_data="sudo_command"),
+                    InlineKeyboardButton("Owner Commands", callback_data="owner_command"),
+                ],[
+                    InlineKeyboardButton("🔙 Go Back", callback_data="home_start")
+                ],
+            ]
+        ),
+    )
